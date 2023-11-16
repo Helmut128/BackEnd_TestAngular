8118c6c1b10dfc71febda8cc6f424a8fd6f6af24
hdr_inventory_20230609 <helmutmartinez128@gmail.com>
2023-11-16 16:40:10 -0600

    Agregando servicios

--- app.js
+++ app.js
@@ -19,13 +19,13 @@ const dbConfig = {
   },
 };
 
-// Endpoint para obtener los usuarios
+// Endpoint para obtener los usuarios "LANGUAGUES"
 app.get("/languages", async (req, res) => {
   try {
     const pool = await mssql.connect(dbConfig);
     const result = await pool
       .request()
-      .query("SELECT [name], [code]  FROM security.[language]");
+      .query("SELECT [name], [code], [idLanguage] FROM security.[language]");
     res.json(result.recordset);
   } catch (error) {
     console.error("Error:", error);
@@ -33,13 +33,13 @@ app.get("/languages", async (req, res) => {
   }
 });
 
-// Endpoint para obtener los usuarios
+// Endpoint para obtener los usuarios "MENU"
 app.get("/menu", async (req, res) => {
   try {
     const pool = await mssql.connect(dbConfig);
     const result = await pool
       .request()
-      .query("SELECT  [name], [url] FROM [security].[menu]");
+      .query("SELECT  [name], [url], [idMenu] FROM [security].[menu]");
     res.json(result.recordset);
   } catch (error) {
     console.error("Error:", error);
@@ -47,7 +47,7 @@ app.get("/menu", async (req, res) => {
   }
 });
 
-// Endpoint para obtener los usuarios
+// Endpoint para obtener los usuarios "PERMISSIONS"
 app.get("/permissions", async (req, res) => {
   try {
     const pool = await mssql.connect(dbConfig);
@@ -61,7 +61,7 @@ app.get("/permissions", async (req, res) => {
   }
 });
 
-// Endpoint para obtener los usuarios
+// Endpoint para obtener los usuarios "AREAS"
 app.get("/areas", async (req, res) => {
   try {
     const pool = await mssql.connect(dbConfig);
@@ -75,7 +75,7 @@ app.get("/areas", async (req, res) => {
   }
 });
 
-// Endpoint para obtener los usuarios
+// Endpoint para obtener los usuarios "CATEGORIES"
 app.get("/categories", async (req, res) => {
   try {
     const pool = await mssql.connect(dbConfig);
@@ -89,13 +89,15 @@ app.get("/categories", async (req, res) => {
   }
 });
 
-// Endpoint para obtener los usuarios
+// Endpoint para obtener los usuarios "USUARIOS"
 app.get("/users", async (req, res) => {
   try {
     const pool = await mssql.connect(dbConfig);
     const result = await pool
       .request()
-      .query("SELECT [name], [userName], [alias] FROM [security].[user]");
+      .query(
+        "SELECT [name], [userName], [alias], [idUser] FROM [security].[user]"
+      );
     res.json(result.recordset);
   } catch (error) {
     console.error("Error:", error);
@@ -210,7 +212,7 @@ app.post("/menu", async (req, res) => {
   }
 });
 
-// Agrega una nueva ruta para actualizar un registro por ID
+// Agrega una nueva ruta para actualizar un registro por ID "AREA"
 app.post("/updateData", async (req, res) => {
   const { idArea } = req.body; //aqui en vez de req.body tenias req.parameter, el body se refiere a todo los datos que le pasaras como objeto o como json y no como url
   const updatedData = req.body;
@@ -228,12 +230,79 @@ app.post("/updateData", async (req, res) => {
     res.json({ success: true });
   } catch (error) {
     console.error("Error:", error);
-    // res.status(500).send("Error de servidor", error);
+    res.status(500).send("Error de servidor", error);
   }
 });
 
+// Agrega una nueva ruta para actualizar un registro por ID "MENU"
+app.post("/updateDataMenu", async (req, res) => {
+  const { idMenu } = req.body;
+  const updatedData = req.body;
+  console.log("Received PUT request for ID:", idMenu);
+
+  try {
+    const pool = await mssql.connect(dbConfig);
+    const result = await pool
+      .request()
+      .input("idMenu", mssql.Int, idMenu)
+      .input("name", mssql.NVarChar, updatedData.name)
+      .query(
+        "UPDATE A SET A.[name] = @name FROM security.menu A WHERE idmenu = @idMenu"
+      );
+    res.json({ success: true });
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).send("Error de servidor", error);
+  }
+});
+
+// Agrega una nueva ruta para actualizar un registro por ID "Lenguage"
+app.post("/updateDataLanguage", async (req, res) => {
+  const { idLanguage } = req.body;
+  const updatedData = req.body;
+  console.log("Received PUT request for ID:", idLanguage);
+
+  try {
+    const pool = await mssql.connect(dbConfig);
+    const result = await pool
+      .request()
+      .input("idLanguage", mssql.Int, idLanguage)
+      .input("name", mssql.NVarChar, updatedData.name)
+      .query(
+        "UPDATE A SET A.[name] = @name FROM security.language A WHERE idLanguage = @idLanguage"
+      );
+    res.json({ success: true });
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).send("Error de servidor", error);
+  }
+});
+
+// Agrega una nueva ruta para actualizar un registro por ID "USERS"
+app.post("/updateDataUser", async (req, res) => {
+  const { idUser } = req.body;
+  const updatedData = req.body;
+  console.log("Received PUT request for ID:", idUser);
+
+  try {
+    const pool = await mssql.connect(dbConfig);
+    const result = await pool
+      .request()
+      .input("idUser", mssql.Int, idUser)
+      .input("name", mssql.NVarChar, updatedData.name)
+      .query(
+        "UPDATE A SET A.[name] = @name FROM security.[user] A WHERE idUser = @idUser"
+      );
+    res.json({ success: true });
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).send("Error de servidor", error);
+  }
+});
+
+//Eliminar DATA de la base de datos "AREA"
 app.delete("/deleteData/:idArea", async (req, res) => {
-  const { idArea } = req.params; //aqui en vez de req.body tenias req.parameter, el body se refiere a todo los datos que le pasaras como objeto o como json y no como url
+  const { idArea } = req.params;
   console.log("Received PUT request for ID:", idArea);
 
   try {
@@ -246,9 +315,116 @@ app.delete("/deleteData/:idArea", async (req, res) => {
     res.json({ success: true });
   } catch (error) {
     console.error("Error:", error);
-    // res.status(500).send("Error de servidor", error);
+    res.status(500).send("Error de servidor", error);
   }
 });
+
+//Eliminar DATA de la base de datos "Menu"
+app.delete("/deleteDataMenu/:idMenu", async (req, res) => {
+  const { idMenu } = req.params;
+  console.log("Received PUT request for ID:", idMenu);
+
+  try {
+    const pool = await mssql.connect(dbConfig);
+    const result = await pool
+      .request()
+      .input("idMenu", mssql.Int, idMenu)
+
+      .query("DELETE FROM security.menu WHERE idMenu = @idMenu");
+    res.json({ success: true });
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).send("Error de servidor", error);
+  }
+});
+
+//Eliminar DATA de la base de datos "Language"
+app.delete("/deleteDataLanguage/:idLanguage", async (req, res) => {
+  const { idLanguage } = req.params;
+  console.log("Received PUT request for ID:", idLanguage);
+
+  try {
+    const pool = await mssql.connect(dbConfig);
+    const result = await pool
+      .request()
+      .input("idLanguage", mssql.Int, idLanguage)
+
+      .query("DELETE FROM security.language WHERE idLanguage = @idLanguage");
+    res.json({ success: true });
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).send("Error de servidor", error);
+  }
+});
+
+//Eliminar DATA de la base de datos "User"
+app.delete("/deleteDataUser/:idUser", async (req, res) => {
+  const { idUser } = req.params;
+  console.log("Received PUT request for ID:", idUser);
+
+  try {
+    const pool = await mssql.connect(dbConfig);
+    const result = await pool
+      .request()
+      .input("idUser", mssql.Int, idUser)
+
+      .query("DELETE FROM security.[user] WHERE idUser = @idUser");
+    res.json({ success: true });
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).send("Error de servidor", error);
+  }
+});
+
+//AGREGANDO INFORMACIÓN A LAS TABLAS
+
+// Endpoint para agregar un nuevo USUARIOS
+app.post("/addUser", async (req, res) => {
+  const { name, userName } = req.body;
+
+  try {
+    const pool = await mssql.connect(dbConfig);
+    const result = await pool
+      .request()
+      .input("name", mssql.NVarChar, name)
+      .input("UserName", mssql.NVarChar, userName)
+      .query(
+        "INSERT INTO [security].[user] ([name]) VALUES (@name); SELECT SCOPE_IDENTITY() AS newUserId;"
+      );
+
+    const newUserId = result.recordset[0].newUserId;
+    res.json({ success: true, newUserId });
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).send("Error de servidor");
+  }
+});
+
+// ...
+
+// Agregar un nuevo área
+app.post("/addArea", async (req, res) => {
+  const { name } = req.body;
+
+  try {
+    const pool = await mssql.connect(dbConfig);
+    const result = await pool
+      .request()
+      .input("name", mssql.NVarChar, name)
+      .input("isActiveLog", mssql.Bit, isActiveLog)
+      .query(
+        "INSERT INTO [security].[area] ([name]) VALUES (@name); SELECT SCOPE_IDENTITY() AS newAreaId;"
+      );
+
+    const newAreaId = result.recordset[0].newAreaId;
+    res.json({ success: true, newAreaId });
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).send("Error de servidor");
+  }
+});
+
+// ...
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
